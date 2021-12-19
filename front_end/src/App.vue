cfcf468cf7e7c7ecfb763ca649b21239b93845a6
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-12-19 22:46:39 +0100

    Implemented NavBar with Bulma

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,4 +1,27 @@
 <template>
+  <nav class="navbar" role="navigation" aria-label="main navigation" style="min-height:5rem;">
+    <div class="navbar-brand" >
+      <a href="/" class="navbar-item is-size-4">LMS</a>
+    </div>
+
+    <div class="navbar-menu" id="navbar-item">
+      <div class="navbar-start">
+        <a href="#" class="navbar-item">Home</a>
+        <a href="#" class="navbar-item">About</a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a href="#" class="button is-primary"><strong>Sign Up</strong></a>
+            <a href="#" class="button is-light">Login</a>
+          </div>
+        </div>
+      </div>
+     
+    </div>
+
+  </nav>
   <router-view/>
 </template>
 
