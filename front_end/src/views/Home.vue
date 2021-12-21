8097f40211d2378ccf0f64c6481d2ef1d7dbed6d
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-12-21 20:20:08 +0100

    Created front page

--- front_end/src/views/Home.vue
+++ front_end/src/views/Home.vue
@@ -1,11 +1,62 @@
 <template>
   <div class="home">
+    <div class="hero is-primary is-medium">
+      <div class="hero-body has-text-centered">
+        <p class="title">Welcome to L.M.S</p>
+        <p class="subtitle">The central location for your child's learning </p>
+
+      </div>
+    </div>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns is-multiline">
+          <div class="column is-4">
+            <div class="box has-text-centered">
+              <span class="icon is-size-2 has-text-success">
+                <i class="far fa-clock"></i>
+              </span>
+              <h2 class="is-size-4 my-4">Learn at your own pace</h2>
+              <p class="is-size-6">When you want</p>
+            </div>
+          </div>
+
+          <div class="column is-4">
+            <div class="box has-text-centered">
+              <span class="icon is-size-2 has-text-success">
+                <i class="far fa-comments"></i>
+              </span>
+              <h2 class="is-size-4 my-4">Learn with others</h2>
+              <p class="is-size-6">Interact with others</p>
+            </div>
+          </div>
+
+          <div class="column is-4">
+            <div class="box has-text-centered">
+              <span class="icon is-size-2 has-text-success">
+                <i class="fas fa-home"></i>
+              </span>
+              <h2 class="is-size-4 my-4">Learn from home</h2>
+              <p class="is-size-6">Wherever you want</p>
+            </div>
+          </div>
+
+          <!-- <div class="buttons has-text-centered">
+            <button class="button is-primary is-large">Click To Get Started</button>
+          </div> -->
+          <div class="column is-12 has-text-centered">
+            <a href="#" class="button is-primary is-size-3 my-6">Click To Get Started</a>
+          </div>
+
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home'
