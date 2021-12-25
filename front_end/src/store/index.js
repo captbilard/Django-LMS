f0dd4c0677fac3c56122e113b6a757a2bc2b433d
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-12-25 22:11:03 +0100

    Implemented store

--- front_end/src/store/index.js
+++ front_end/src/store/index.js
@@ -2,8 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: {
+      token: null,
+      isAuthenticated: false
+    }
   },
   mutations: {
+    initializeStore: function (state) {
+      if (localStorage.getItem('token')) {
+        state.user.token = localStorage.getItem('token')
+        state.user.isAuthenticated = true
+      } else {
+        state.user.token = null;
+        state.user.isAuthenticated = false;
+      }
+    },
+    setToken: function (state, token) {
+      state.user.token = token
+      state.user.isAuthenticated = false
+    },
+    removeToken: function (state) {
+      state.user.token = null
+      state.user.isAuthenticated = false
+    }
   },
   actions: {
   },
