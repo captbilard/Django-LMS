b3cdf3e20f330b09402bce409332d4da75ef6afa
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-12-25 16:50:59 +0100

    Implemented Form validation

--- front_end/src/views/SignUp.vue
+++ front_end/src/views/SignUp.vue
@@ -10,11 +10,11 @@
             <div class="container">
                 <div class="columns">
                     <div class="column is-4 is-offset-4">
-                        <form v-on:submit.prevent="onSubmit">
+                        <form v-on:submit.prevent="checkForm">
                             <div class="field">
                                 <label for="username" class="label">Username</label>
                                 <div class="control has-icons-left">
-                                    <input type="text" name="username" id="" class="input">
+                                    <input type="text" name="username" class="input" v-model="username">
                                     <span class="icon is-small is-left">
                                         <i class="fas fa-user"></i>
                                     </span>
@@ -24,7 +24,7 @@
                             <div class="field">
                                 <label for="email" class="label">Email</label>
                                 <div class="control has-icons-left">
-                                    <input type="email" name="email" id="" class="input">
+                                    <input type="email" name="email" class="input" v-model="email">
                                     <span class="icon is-small is-left">
                                         <i class="fas fa-envelope"></i>
                                     </span>
@@ -34,7 +34,7 @@
                             <div class="field">
                                 <label for="password1" class="label">Password</label>
                                 <div class="control has-icons-left">
-                                    <input type="password" name="password1" id="" class="input">
+                                    <input type="password" name="password1" class="input" v-model="password">
                                     <span class="icon is-small is-left">
                                         <i class="fas fa-key"></i>
                                     </span>
@@ -44,11 +44,18 @@
                             <div class="field">
                                 <label for="password2" class="label">Confirm Your Password</label>
                                 <div class="control has-icons-left">
-                                    <input type="password" name="password2" id="" class="input">
+                                    <input type="password" name="password2" class="input" v-model="password2">
                                     <span class="icon is-small is-left">
                                         <i class="fas fa-key"></i>
                                     </span>
                                 </div>
+                            </div>
+
+                            <div class=" notification is-danger" v-if="errors.length">
+                                <button class="delete" v-on:click="clearError"></button>
+                                <p v-for="error in errors" v-bind:key="error">
+                                    {{ error }}
+                                </p>
                             </div>
 
                             <div class="field">
@@ -69,10 +76,56 @@
 
 <script>
 export default {
-    methods:{
-        onSubmit(){
-            console.log("This is submitted")
+    data(){
+        return{
+            errors: [],
+            username:null,
+            email: null,
+            password:null,
+            password2:null,
         }
+    },
+    methods:{
+        checkForm: function(){
+            this.errors = []
+
+            if(!this.username){
+                this.errors.push("Username is required")
+            }
+            if(!this.email){
+                this.errors.push("Email is required")
+            }else if(!this.validEmail(this.email)){
+                this.errors.push("Valid email is required")
+            }
+            if(!this.password){
+                this.errors.push("Password is required")
+            } else if(!this.validPassword(this.password)){
+                this.errors.push("Password doesn't meet the minimum requirements")
+            }
+            if(!this.password2 && this.password !== this.password2){
+                this.errors.push("Passwords don't match")
+            }
+            // if(!this.errors.length){
+            //     submitForm()
+            // }
+            // console.log("This is submitted")
+        },
+
+        validEmail: function(email){
+            let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email)
+        },
+
+        validPassword: function(password){
+            let re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+            return re.test(password)
+        },
+
+        clearError: function (){
+            const notification =  document.querySelector('.notification')
+            notification.parentNode.removeChild(notification)
+            console.log("This should be deleted");
+        },
     }
 }
 </script>
