60b7271258c548a331ec7c11e901e3349fb340cb
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-12-27 21:13:33 +0100

    Create basic account page with logout functionality

---
+++ front_end/src/views/dashboard/MyAccount.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="myAccount">
+        <div class="hero is-primary">
+            <div class="hero-body has-text-centered">
+                <p class="title">My Account</p>
+            </div>
+        </div>
+
+        <section class="section">
+            <button class="button is-danger" v-on:click="logout">Logout</button>
+        </section>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    methods:{
+        logout: function (){
+            delete axios.defaults.headers.common["Authorization"]
+
+            //remove token from 
+            localStorage.removeItem('token')
+
+            //remove from store using mutations
+            this.$store.commit('removeToken')
+
+            this.$router.push('/')
+        }
+    }
+}
+</script>
