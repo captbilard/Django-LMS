a1d58b45b9143298f852b81381e7ffd4c0f304b5
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-12-30 02:50:56 +0100

    Displayed data on course page

--- front_end/src/views/Course.vue
+++ front_end/src/views/Course.vue
@@ -2,7 +2,7 @@
     <div class="course">
         <div class="hero is-primary">
             <div class="hero-body has-text-centered">
-                <p class="title">The title of the course</p>
+                <p class="title"> {{course.title}} </p>
             </div>
         </div>
 
@@ -25,11 +25,9 @@
                     <div class="column is-10">
                         <h3>Introduction</h3>
                         <p class="has-text-justified">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet justo donec enim diam vulputate ut pharetra sit. At volutpat diam ut venenatis tellus in. Vestibulum morbi blandit cursus risus. Lectus mauris ultrices eros in cursus turpis. Arcu ac tortor dignissim convallis aenean et tortor at risus. Aliquet risus feugiat in ante metus dictum at. Tortor dignissim convallis aenean et. Turpis nunc eget lorem dolor sed. At in tellus integer feugiat scelerisque varius morbi. 
+                            {{ course.long_description }}
                         </p>
-                        <p class="has-text-justified">
-                            Nec dui nunc mattis enim. Amet consectetur adipiscing elit duis. Consectetur a erat nam at lectus urna duis. Dictum at tempor commodo ullamcorper a lacus vestibulum. Volutpat ac tincidunt vitae semper quis. Ut venenatis tellus in metus vulputate eu scelerisque felis. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Massa vitae tortor condimentum lacinia quis vel eros donec.
-                        </p>
+                        
 
                         <h3>Getting Started</h3>
                         <p class="has-text-justified">
