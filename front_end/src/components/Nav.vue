879babbce44eab1cfdac69393bbeebf46e5239a0
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-01-01 19:03:45 +0100

    Included link to courses - modified all navbar `a` item to router-link

--- front_end/src/components/Nav.vue
+++ front_end/src/components/Nav.vue
@@ -1,29 +1,38 @@
 <template>
-    <nav class="navbar is-primary" role="navigation" aria-label="main navigation" style="min-height:5rem;">
-        <div class="navbar-brand" >
-            <a href="/" class="navbar-item is-size-4">LMS</a>
-        </div>
+  <nav
+    class="navbar is-primary"
+    role="navigation"
+    aria-label="main navigation"
+    style="min-height: 5rem"
+  >
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item is-size-4">LMS</router-link>
+    </div>
 
-        <div class="navbar-menu" id="navbar-item">
-            <div class="navbar-start">
-                <a href="/" class="navbar-item">Home</a>
-                <a href="/about" class="navbar-item">About</a>
-            </div>
+    <div class="navbar-menu" id="navbar-item">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Home</router-link>
+        <router-link to="/about" class="navbar-item">About</router-link>
+        <router-link to="/courses" class="navbar-item">Courses</router-link>
+      </div>
 
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <template v-if="$store.state.user.isAuthenticated">
-                            <router-link to="/dashboard/my-account" class="button is-warning"> My Dashboard</router-link>
-                        </template>
-                        <template v-else>
-                            <a href="/sign-up" class="button is-primary"><strong>Sign Up</strong></a>
-                            <a href="/login" class="button is-light">Login</a>
-                        </template>
-                    </div>
-                </div>
-            </div>
-        
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <template v-if="$store.state.user.isAuthenticated">
+              <router-link to="/dashboard/my-account" class="button is-warning">
+                My Dashboard</router-link
+              >
+            </template>
+            <template v-else>
+              <a href="/sign-up" class="button is-primary"
+                ><strong>Sign Up</strong></a
+              >
+              <a href="/login" class="button is-light">Login</a>
+            </template>
+          </div>
         </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 </template>
--- front_end/src/views/Course.vue
+++ front_end/src/views/Course.vue
@@ -1,64 +1,59 @@
 <template>
-    <div class="course">
-        <div class="hero is-primary">
-            <div class="hero-body has-text-centered">
-                <p class="title"> {{course.title}} </p>
-            </div>
-        </div>
-
-        <section class="section">
-            <div class="container">
-                <div class="columns content">
-                    <div class="column is-2">
-                        <h3>Table of Contents</h3>
-                        <ul>
-                            <li><a href="#">Introduction</a></li>
-                            <li><a href="#">Getting Started</a></li>
-                            <li><a href="#">Part 1</a></li>
-                            <li><a href="#">Part 2</a></li>
-                            <li><a href="#">Part 3</a></li>
-                            <li><a href="#">Summary</a></li> 
-
-                        </ul>
-                    </div>
-
-                    <div class="column is-10">
-                        <template v-if="$store.state.user.isAuthenticated">
-                            <h3>Introduction</h3>
-                            <p class="has-text-justified">
-                                {{ course.long_description }}
-                            </p>
-                        </template>
-                        <template v-else>
-                            <h2>Restricted Access</h2>
-                            <p>
-                                Kindly Sign Up or Login to access course content
-                            </p>
-                        </template>
-                    </div>
-                </div>
-            </div>
-        </section>
+  <div class="course">
+    <div class="hero is-primary">
+      <div class="hero-body has-text-centered">
+        <p class="title">{{ course.title }}</p>
+      </div>
     </div>
-    
+
+    <section class="section">
+      <div class="container">
+        <div class="columns content">
+          <div class="column is-2">
+            <h3>Table of Contents</h3>
+            <ul>
+              <li><a href="#">Introduction</a></li>
+              <li><a href="#">Getting Started</a></li>
+              <li><a href="#">Part 1</a></li>
+              <li><a href="#">Part 2</a></li>
+              <li><a href="#">Part 3</a></li>
+              <li><a href="#">Summary</a></li>
+            </ul>
+          </div>
+
+          <div class="column is-10">
+            <template v-if="$store.state.user.isAuthenticated">
+              <h3>Introduction</h3>
+              <p class="has-text-justified">
+                {{ course.long_description }}
+              </p>
+            </template>
+            <template v-else>
+              <h2>Restricted Access</h2>
+              <p>Kindly Sign Up or Login to access course content</p>
+            </template>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-  data(){
-    return{
-      course: []
-    }
+  data() {
+    return {
+      course: [],
+    };
   },
-  mounted(){
-    const baseUrl = "http://127.0.0.1:8000"
-    const slug = this.$route.params.slug
-    axios.get(`${baseUrl}/api/v1/courses/${slug}`)
-    .then(response =>{
-      this.course = response.data
+  mounted() {
+    const baseUrl = "http://127.0.0.1:8000";
+    const slug = this.$route.params.slug;
+    axios.get(`${baseUrl}/api/v1/courses/${slug}`).then((response) => {
+      this.course = response.data;
       console.log(response.data);
-    })
-  }
-}
+    });
+  },
+};
 </script>
