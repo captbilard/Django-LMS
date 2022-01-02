2e5df10eb92c6f511ca885fc390592487a27adf0
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-01-02 14:46:44 +0100

    Created Lessons model in backend

--- lms/courses/admin.py
+++ lms/courses/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Categories, Courses
+from .models import Categories, Courses, Lessons
 
 # Register your models here.
 admin.site.register(Categories)
 admin.site.register(Courses)
+admin.site.register(Lessons)
