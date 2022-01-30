8cb78a2190d477c9c57c97c36b0195f22ae07a11
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-01-30 20:38:02 +0100

    Get all categories from db

--- lms/courses/views.py
+++ lms/courses/views.py
@@ -5,11 +5,17 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework import serializers, status
 
-from .models import Courses, Lessons, Comments
-from courses.serializers import CommentsSerializer, CourseListSerializer, CourseDetailSerializer, LessonListSerializer
+from .models import Courses, Lessons, Comments, Categories
+from courses.serializers import CommentsSerializer, CourseListSerializer, CourseDetailSerializer, LessonListSerializer, CategoriesSerializer
 
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_categories(request):
+    categories = Categories.objects.all()
+    serializer = CategoriesSerializer(categories, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
