9468b5776e3087715e4ac1b05c6d805e7077b4f4
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-02-03 21:46:51 +0100

    Create view to handle quiz endpoint

--- lms/courses/serializers.py
+++ lms/courses/serializers.py
@@ -6,30 +6,34 @@ from .models import Comments, Courses, Lessons, Categories, Quiz
 class CategoriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categories
-        fields = ('id','title', 'slug')
+        fields = ("id", "title", "slug")
+
 
 class CourseListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Courses
-        fields = ('id','title','slug', 'short_description', 'get_image')
+        fields = ("id", "title", "slug", "short_description", "get_image")
+
 
 class CourseDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Courses
-        fields = ('id','title','slug', 'short_description', 'long_description')
+        fields = ("id", "title", "slug", "short_description", "long_description")
+
 
 class LessonListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lessons
-        fields = ('id','title','slug', 'short_description', 'long_description')
+        fields = ("id", "title", "slug", "short_description", "long_description")
 
 
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
-        fields = ('id','name','content','created_at')
+        fields = ("id", "name", "content", "created_at")
+
 
 class QuizSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quiz
-        fields = ('id', 'lesson', 'question','answer','option1','option2','option3')
+        fields = ("id", "lesson", "question", "answer", "option1", "option2", "option3")
