8a558597d72a1823af219bfad06671097c2788ed
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-02-05 01:03:12 +0100

    Wrote test for 'get_courses' endpoint

--- lms/courses/tests.py
+++ lms/courses/tests.py
@@ -1,6 +1,5 @@
 # from django.test import TestCase
 import json
-from unicodedata import category
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -19,7 +18,10 @@ from courses.serializers import (
 
 class CoursesTest(APITestCase):
     def setUp(self):
-        Categories.objects.create(title="design", slug="design-course")
+        self.category = Categories.objects.create(title="design", slug="design-course")
+        self.course = Courses.objects.create(title="The design course",slug="the-design-course")
+        self.course.category.add(self.category)
+
     def test_get_categories(self):
         """
         Ensures that the endpoints returns 
@@ -27,7 +29,18 @@ class CoursesTest(APITestCase):
         """
         url = reverse("get_categories")
         serializer = CategoriesSerializer(Categories.objects.all(), many=True)
-        response = self.client.get(url,format='json')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+    
+    def test_get_courses(self):
+        """
+        Ensures that the endpoints returns 
+        all courses
+        """
+        url = reverse("get_courses")
+        serializer = CourseListSerializer(Courses.objects.all(), many=True)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
 
