ced74a1e33ae80a94e2e9458b0f942688b8a36d8
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-02-05 20:58:47 +0100

    Wrote test for frontpage courses endpoint

--- lms/courses/tests.py
+++ lms/courses/tests.py
@@ -20,7 +20,15 @@ class CoursesTest(APITestCase):
     def setUp(self):
         self.category = Categories.objects.create(title="design", slug="design-course")
         self.course = Courses.objects.create(title="The design course",slug="the-design-course")
+        self.course1 = Courses.objects.create(title="Course1",slug="course-1")
+        self.course2 = Courses.objects.create(title="Course2",slug="course-2")
+        self.course3 = Courses.objects.create(title="Course3",slug="course-3")
+        self.course4 = Courses.objects.create(title="Course4",slug="course-4")
         self.course.category.add(self.category)
+        self.course1.category.add(self.category)
+        self.course2.category.add(self.category)
+        self.course3.category.add(self.category)
+        self.course4.category.add(self.category)
 
     def test_get_categories(self):
         """
@@ -43,4 +51,14 @@ class CoursesTest(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
+
+    def test_get_frontpage_courses(self):
+        """
+        Ensures only the top 4 courses is returned 
+        """
+        url = reverse("get_frontpage_courses")
+        serializer = CourseListSerializer(Courses.objects.all()[0:4], many=True)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertCountEqual(response.data, serializer.data)
 
