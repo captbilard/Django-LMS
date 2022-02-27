f7feaa37f2a187262ad33f3019fb1554e94ccf0e
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-02-27 16:32:17 +0100

    Successfully Integrated stripe with application.

--- front_end/src/views/Stripe_Payments/SuccessPage.vue
+++ front_end/src/views/Stripe_Payments/SuccessPage.vue
@@ -5,19 +5,30 @@
         <p class="title">Payment Successful</p>
       </div>
     </div>
-    <section class="section">
-      <p>Welcome to the Premium side</p>
-      <hr />
-      <router-link to="/courses">Click here</router-link> to view all courses!
-    </section>
+    <template v-if="$store.state.user.isAuthenticated">
+      <section class="section">
+        <p>Welcome to the Premium side</p>
+        <hr />
+        <router-link to="/courses">Click here</router-link> to view all courses!
+      </section>
+    </template>
+    <template v-else>
+      <section class="section">
+        <p>Oops you need to be logged in to access this page</p>
+      </section>
+    </template>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   async mounted() {
-    await axios.get("/create_premium_users/").then((response) => {
+    await axios.get("/api/v1/courses/create_premium_user/").then((response) => {
       console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
     });
     document.title = `Successful | LMS`;
   },
